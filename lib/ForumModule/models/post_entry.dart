ed14a1cd38ee2b9592fbd70b154e3755361e42cd
“keiraislearning” <Keirana18@gmail.com>
2025-12-20 20:00:53 +0700

    fix: unimportant import

--- lib/ForumModule/models/post_entry.dart
+++ lib/ForumModule/models/post_entry.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final postEntry = postEntryFromJson(jsonString);
-
 import 'dart:convert';
 
 List<PostEntry> postEntryFromJson(String str) => List<PostEntry>.from(json.decode(str).map((x) => PostEntry.fromJson(x)));
